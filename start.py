672ac2a0c880512eb608c7015ed721dec4458b63
dkrivaa <dannykrivaa@hotmail.com>
2023-06-11 20:42:31 +0300

    Basic app

--- start.py
+++ start.py
@@ -4,15 +4,21 @@ import altair as alt
 import numpy as np
 from dataframemaker import df, df_quarter, latest_year, latest_quarter
 
-
 def start_data(window_width):
     st.write("""
     ### The data at a glance
     """)
-    # Making three columns
-    col1, col2, col3 = st.columns(3)
-    # ANNUAL TOTAL DATA NATIONWIDE
-    with col1:
+
+    # Setting the number of columns according to width of window being used
+    if window_width <= 600:
+        ncol = 1
+    elif 600 < window_width <= 1100:
+        ncol = 2
+    elif window_width > 1100:
+        ncol = 3
+
+    # The graph functions
+    def start1():
         try:
             st.markdown("""
             **Total cases by year**
@@ -23,15 +29,15 @@ def start_data(window_width):
                                   })
             total['x'] = total['x'].astype(str)
             # Defining the chart
-            chart = alt.Chart(total).mark_bar(size=window_width*0.025).encode(
+            chart = alt.Chart(total).mark_bar(size=window_width * 0.025).encode(
                 x=alt.X('x', axis=alt.Axis(title='year')),
-                y=alt.Y('y', axis=alt.Axis(title='cases')),)
+                y=alt.Y('y', axis=alt.Axis(title='cases')), )
             # Rendering the chart
             st.altair_chart(chart, use_container_width=True)
         except:
             st.write('**NO DATA**')
 
-    with col2:
+    def start2():
         try:
             st.markdown(f"""
             **Total cases by Police district: {latest_quarter} {latest_year}**
@@ -42,17 +48,15 @@ def start_data(window_width):
                                   'x': (sorted(dfi['PoliceDistrict'].unique().tolist()))
                                   })
             # Defining the chart
-            chart = alt.Chart(total).mark_bar(size=window_width*0.025, color='orange').encode(
+            chart = alt.Chart(total).mark_bar(size=window_width * 0.025, color='orange').encode(
                 x=alt.X('x', axis=alt.Axis(title='year')),
-                y=alt.Y('y', axis=alt.Axis(title='cases')),)
+                y=alt.Y('y', axis=alt.Axis(title='cases')), )
             # Rendering the chart
             st.altair_chart(chart, use_container_width=True)
         except:
             st.write('**NO DATA**')
 
-
-    # LATEST QUARTER CRIMES BY TYPE OF MUICIPALITY
-    with col3:
+    def start3():
         try:
             st.write(f"""
             **Total cases by type of municipality: {latest_quarter} {latest_year}**  
@@ -64,9 +68,9 @@ def start_data(window_width):
                                   })
             # Revaluing the categories of types of municipalities
             total['category'] = total['category'].map(lambda x: 'Jewish' if x == 1
-                                                                else 'Mixed' if x == 2
-                                                                else 'Arab')
-            chart = alt.Chart(total).mark_arc(innerRadius=window_width*0.05).encode(
+            else 'Mixed' if x == 2
+            else 'Arab')
+            chart = alt.Chart(total).mark_arc(innerRadius=window_width * 0.05).encode(
                 theta='value',
                 color=alt.Color('category:N', scale=alt.Scale(scheme='category10')),
             )
@@ -75,7 +79,7 @@ def start_data(window_width):
         except:
             st.write('**NO DATA**')
 
-    with col1:
+    def start4():
         try:
             st.markdown("""
             **Total cases by year (same period)**
@@ -85,10 +89,10 @@ def start_data(window_width):
                                   'x': (sorted(df_quarter['year'].unique().tolist()))
                                   })
             total['x'] = total['x'].astype(str)
-            y_max = 1.05*max(list(df.groupby(df_quarter['year'])['TikimSum'].sum().values))
-            y_min = 0.95*min(list(df.groupby(df_quarter['year'])['TikimSum'].sum().values))
+            y_max = 1.05 * max(list(df.groupby(df_quarter['year'])['TikimSum'].sum().values))
+            y_min = 0.95 * min(list(df.groupby(df_quarter['year'])['TikimSum'].sum().values))
             # Defining the chart
-            chart = alt.Chart(total).mark_bar(size=window_width*0.025).encode(
+            chart = alt.Chart(total).mark_bar(size=window_width * 0.025).encode(
                 x=alt.X('x', axis=alt.Axis(title='year')),
                 y=alt.Y('y', axis=alt.Axis(title='cases'), scale=alt.Scale(domain=(y_min, y_max), clamp=True)), )
             # Rendering the chart
@@ -96,8 +100,7 @@ def start_data(window_width):
         except:
             st.write('**NO DATA**')
 
-    # Total murders by quarter
-    with col2:
+    def start5():
         try:
             st.markdown("""
             **Total murders by quarter**
@@ -117,8 +120,7 @@ def start_data(window_width):
         except:
             st.write('**NO DATA**')
 
-    # Total car thefts by quarter
-    with col3:
+    def start6():
         try:
             st.markdown("""
             **Total car thefts by quarter**
@@ -138,3 +140,19 @@ def start_data(window_width):
         except:
             st.write(' **NO DATA**')
 
+    # Making the columns
+    cols = st.columns(ncol)
+    # Making list of the graph functions
+    functions = [start1, start2, start3, start4, start5, start6]
+
+    num_functions = len(functions)
+
+    column_index = 0
+    col_list = st.columns(ncol)
+
+    for i in range(num_functions):
+        column_index = i % ncol
+
+        with col_list[column_index]:
+            st.subheader(f"Function {i + 1}")
+            functions[i]()
